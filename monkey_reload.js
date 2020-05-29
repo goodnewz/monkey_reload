0675f0e0b9566a14f1ff3f04339ae1173cad9776
Cvet <cvet84@gmail.com>
2020-05-28 20:34:33 -0400

    Making it prettier to look at. Removing run counts (date and time of last reload is much more useful)

--- monkey_reload.js
+++ monkey_reload.js
@@ -1,33 +1,51 @@
 // ==UserScript==
 // @name         Monkey reload
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Reloads a page it on every X minutes to prevent automatic logout. 
 // @author       Cvet Georgiev
 // @match        https://outlook.office.com/calendar/view/workweek
 // @match        https://www.office.com/?auth=2
 // @grant        none
 
+
 // ==/UserScript==
+var interval = 10; // set interval in munutes
 
 var block_to_insert ;
 block_to_insert = document.createElement( 'div' );
-var number_times = 'Number of times reloaded ';
-var date_time=". Reloaded last at: "
+block_to_insert.style.background="black";
+block_to_insert.style.height="21px"
+block_to_insert.style.color="red";
+var date_time="Reloaded last on: ";
 var today = new Date();
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
 
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
 
-function count_times_reloaded(){
-    if (localStorage.counter) {
-        localStorage.counter = Number(localStorage.counter) + 1;
-    } else {
-        localStorage.counter = 1;
-    }
+function reload(){
     location.reload();
 }
-setInterval(count_times_reloaded, 10*60*1000);// realod the page every 10 minutes
 
-block_to_insert.innerHTML = number_times.concat(localStorage.counter,date_time,today.getHours(),":",today.getMinutes(),":",today.getSeconds()) ;
-var body=document.getElementsByTagName('body')[0];
+setInterval(reload, interval*60*1000);// realod the page every 10 minutes
+
+block_to_insert.innerHTML = date_time.concat(month[today.getMonth()]," ",today.getDay(),", ",today.getFullYear()," at ",addZero(today.getHours()),":",addZero(today.getMinutes()),":",addZero(today.getSeconds()));
+var body = document.getElementsByTagName('body')[0];
 body.insertBefore(block_to_insert,body.childNodes[0]);
-//localStorage.removeItem("counter");
